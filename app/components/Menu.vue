dd840c1edf8b3538402349ab1f73d193b8f0713b
Denis Danilov <d.danilov@duotek.ru>
2025-11-10 20:16:17 +0300

    Added menu

--- app/components/Menu.vue
+++ app/components/Menu.vue
@@ -1,30 +1,156 @@
 <script setup lang="ts">
+import {
+  ElIconLocation,
+  ElIconTickets
+} from '#imports'
+import { type Component } from 'vue'
 
+const props = defineProps<{
+  height?: string
+}>()
+
+const router = useRouter()
+
+interface MenuItem {
+  name: string,
+  icon: Component,
+  children?: MenuItem[]
+  link: string
+}
+
+const menuItems: MenuItem[] = [
+  {
+    name: 'Шумы',
+    icon: ElIconLocation,
+    children: [
+      {
+        name: 'Каталог треков',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Коллекции',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Авторы',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Библиотеки',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+    ]
+  },
+  {
+    name: 'Музыка',
+    icon: ElIconTickets,
+    children: [
+      {
+        name: 'Каталог треков',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Плейлисты',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Авторы',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Лейблы',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Альбомы',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+    ]
+  },
+  {
+    name: 'Пользователи',
+    icon: ElIconLocation,
+    children: [
+      {
+        name: 'Каталог пользователей',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+      {
+        name: 'Отчеты',
+        link: '/example',
+        icon: ElIconLocation,
+      },
+    ]
+  },
+  {
+    name: 'Блог',
+    link: '/example',
+    icon: ElIconLocation,
+  },
+  {
+    name: 'Ручки',
+    link: '/example',
+    icon: ElIconLocation,
+  },
+  {
+    name: 'Ручки',
+    link: '/example',
+    icon: ElIconLocation,
+  },
+  {
+    name: 'Ручки',
+    link: '/example',
+    icon: ElIconLocation,
+  },
+  {
+    name: 'Ручки',
+    link: '/example',
+    icon: ElIconLocation,
+  },
+]
+
+const onClickLink = (menuItem: MenuItem) => {
+  router.push(menuItem.link)
+}
 </script>
 
 <template>
-  <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-  >
-    <el-sub-menu index="1">
-      <template #title>
-        <el-icon>
-          <el-icon-location />
-        </el-icon>
-        <span>Navigator One</span>
+  <el-menu class="side-menu" :unique-opened="true">
+    <el-scrollbar :height="props.height ?? '100vh'" :always="true">
+      <template v-for="(menuItem, i) in menuItems">
+        <el-sub-menu class="sub-menu" v-if="menuItem.children" :index="i.toString()" >
+          <template #title>
+            <el-icon>
+              <component v-if="menuItem.icon" :is="menuItem.icon" />
+            </el-icon>
+            <span>{{ menuItem.name }}</span>
+          </template>
+          <template #default>
+            <el-menu-item v-for="(menuItemChildren, j) in menuItem.children" :index="i.toString() + '-' + j.toString()">
+              <el-icon>
+                <component v-if="menuItemChildren.icon" :is="menuItemChildren.icon" />
+              </el-icon>
+              <span @click="onClickLink(menuItemChildren)">{{ menuItemChildren.name }}</span>
+            </el-menu-item>
+          </template>
+        </el-sub-menu>
+        <el-menu-item v-else :index="i.toString()">
+          <el-icon>
+            <component v-if="menuItem.icon" :is="menuItem.icon" />
+          </el-icon>
+          <span @click="onClickLink(menuItem)">{{ menuItem.name }}</span>
+        </el-menu-item>
       </template>
-      <el-menu-item-group title="Group One">
-        <el-menu-item index="1-1">item one</el-menu-item>
-        <el-menu-item index="1-2">item two</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="Group Two">
-        <el-menu-item index="1-3">item three</el-menu-item>
-      </el-menu-item-group>
-      <el-sub-menu index="1-4">
-        <template #title>item four</template>
-        <el-menu-item index="1-4-1">item one</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
+    </el-scrollbar>
   </el-menu>
 </template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,14 +1,69 @@
 <script setup lang="ts">
 import Menu from "~/components/Menu.vue"
+
+let ro = undefined
+
+const router = useRouter()
+
+const logoContainerRef = useTemplateRef('logoContainerRef')
+const footerRef = useTemplateRef('footerRef')
+
+const scrollbarHeight = ref()
+
+const updateMenuDimensions = () => {
+  scrollbarHeight.value = (window.innerHeight - logoContainerRef.value.offsetHeight - footerRef.value.offsetHeight) + 'px'
+}
+
+const initResizeObserver = () => {
+  const body = document.body;
+  let lastHeight = body.offsetHeight;
+
+  const observer = new ResizeObserver(entries => {
+    for (const entry of entries) {
+      const newHeight = entry.contentRect.height;
+      if (newHeight !== lastHeight) {
+        updateMenuDimensions()
+        lastHeight = newHeight;
+      }
+    }
+  });
+
+  observer.observe(body);
+
+  return observer
+}
+
+const clickHome = () => {
+  router.push('/')
+}
+
+onMounted(() => {
+  ro = initResizeObserver()
+  updateMenuDimensions()
+})
+
+onUnmounted(() => {
+  ro.unobserve()
+})
 </script>
 
 <template>
   <el-scrollbar height="100vh">
     <el-container class="el-container">
-      <el-aside>
-        <Menu />
+      <el-aside class="side-menu__container">
+        <div class="side-menu__logo" ref="logoContainerRef" @click="clickHome">
+          <img src="/img/logo.png" alt="Logo">
+          <div>Digital Dyatel</div>
+        </div>
+        <Menu :height="scrollbarHeight"/>
+        <div class="side-menu__footer" ref="footerRef">
+          <img src="/img/avatar.png" alt="avatar">
+          <p class="username">Денис Данилов</p>
+          <p class="role">Администратор</p>
+          <a>Выйти</a>
+        </div>
       </el-aside>
-      <el-container>
+      <el-container class="main-container">
         <el-main>
           <slot/>
         </el-main>
