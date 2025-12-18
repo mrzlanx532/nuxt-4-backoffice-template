a6320d71083300d949391996c36c57e24085e196
Denis Danilov <d.danilov@duotek.ru>
2025-12-18 18:26:43 +0300

    Temp

---
+++ app/components/Section.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useSlots } from 'vue'
+
+const slot = useSlots()
+</script>
+
+<template>
+  <div class="section">
+    <div class="section__header" v-if="slot.header">
+      <slot name="header"/>
+    </div>
+    <div class="section__content">
+      <slot name="content"/>
+    </div>
+  </div>
+</template>
--- app/pages/blog/_tabs/main.vue
+++ app/pages/blog/_tabs/main.vue
@@ -1,7 +1,29 @@
 <script setup lang="ts">
+import SectionGroup from '@/components/SectionGroup.vue'
+import Section from '@/components/Section.vue'
 
+interface IBlogPost {
+  cover: {
+    original: string
+  } | null
+  content_short: string | null,
+  content: string | null
+}
+
+const props = defineProps<{
+  item?: IBlogPost
+}>()
 </script>
 
 <template>
-  <div>1532452345423</div>
+  <SectionGroup v-if="props.item">
+    <Section>
+      <template #header>Краткое содержание</template>
+      <template #content>{{ props.item.content_short ?? '[Не заполнено]' }}</template>
+    </Section>
+    <Section>
+      <template #header>Полное содержание</template>
+      <template #content><div v-html="props.item.content ?? '[Не заполнено]'" /></template>
+    </Section>
+  </SectionGroup>
 </template>
