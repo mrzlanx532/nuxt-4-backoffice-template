542153119fa0c2da9a895f3b93a8acfb2724ac23
mrzlanx532 <denis_danilov_97@mail.ru>
2025-12-26 17:28:49 +0300

    hot fix

--- app/modal/UserFormModal.vue
+++ app/modal/UserFormModal.vue
@@ -11,6 +11,11 @@ interface BlogPostFormResponse {
   locales: []
 }
 
+interface Locale {
+  id: string
+  title: string
+}
+
 const props = defineProps<{
   id?: number
 }>()
@@ -19,16 +24,21 @@ const emit = defineEmits<{
   (e: 'close'): void,
 }>()
 
+const locales = ref<Locale[]>([])
+
 const { $authFetch } = useNuxtApp()
 
 const formData = ref<{[key: string]: any}>({
+  first_name: undefined,
+  last_name: undefined,
+  email: undefined,
+  phone: undefined,
+  password: undefined,
+  password_confirmation: undefined,
   locale_id: undefined,
-  category_id: undefined,
-  name: undefined,
-  date: undefined,
-  cover: undefined,
-  content_short: undefined,
-  content: undefined
+  about: undefined,
+  picture: undefined,
+  is_checked: false
 })
 
 const {
@@ -56,6 +66,8 @@ onMounted(async () => {
     formData.value = response.entity
   }
 
+  locales.value = response.locales
+
   isReady.value = true
 })
 </script>
@@ -63,32 +75,48 @@ onMounted(async () => {
 <template>
   <FormModal @save="save" :is-ready="isReady">
     <el-form label-position="top">
-      <el-form-item-with-error label="Имя" name="first_name" :errors="errors">
-        <el-input v-model="formData.first_name" />
-      </el-form-item-with-error>
-      <el-form-item-with-error label="Фамилия" name="last_name" :errors="errors">
-        <el-input v-model="formData.last_name" />
-      </el-form-item-with-error>
-      <el-form-item-with-error label="Email" name="email" :errors="errors">
-        <el-input v-model="formData.email" />
-      </el-form-item-with-error>
-      <el-form-item-with-error label="Телефон" name="phone" :errors="errors">
-        <el-input v-model="formData.phone" />
-      </el-form-item-with-error>
-      <el-form-item-with-error label="Пароль" name="password" :errors="errors">
-        <el-input v-model="formData.password" />
-      </el-form-item-with-error>
-      <el-form-item-with-error label="Подтверждение пароля" name="password_confirmation" :errors="errors">
-        <el-input v-model="formData.password_confirmation" />
-      </el-form-item-with-error>
+      <el-space>
+        <el-form-item-with-error label="Имя" name="first_name" :errors="errors">
+          <el-input v-model="formData.first_name" />
+        </el-form-item-with-error>
+        <el-form-item-with-error label="Фамилия" name="last_name" :errors="errors">
+          <el-input v-model="formData.last_name" />
+        </el-form-item-with-error>
+      </el-space>
+      <el-space>
+        <el-form-item-with-error label="Email" name="email" :errors="errors">
+          <el-input v-model="formData.email" />
+        </el-form-item-with-error>
+        <el-form-item-with-error label="Телефон" name="phone" :errors="errors">
+          <el-input v-model="formData.phone" />
+        </el-form-item-with-error>
+      </el-space>
+      <el-space>
+        <el-form-item-with-error label="Пароль" name="password" :errors="errors">
+          <el-input type="password" v-model="formData.password" />
+        </el-form-item-with-error>
+        <el-form-item-with-error label="Подтверждение пароля" name="password_confirmation" :errors="errors">
+          <el-input type="password" v-model="formData.password_confirmation" />
+        </el-form-item-with-error>
+      </el-space>
       <el-form-item-with-error label="Язык" name="locale_id" :errors="errors">
-        <el-input v-model="formData.locale_id" />
+        <el-select v-model="formData.locale_id">
+          <el-option
+              v-for="locale in locales"
+              :key="locale.id"
+              :label="locale.title"
+              :value="locale.id"
+          />
+        </el-select>
       </el-form-item-with-error>
       <el-form-item-with-error label="О себе" name="about" :errors="errors">
         <Textarea v-model="formData.about" />
       </el-form-item-with-error>
       <el-form-item-with-error label="Фотография" name="picture" :errors="errors">
         <InputFile v-model="formData.picture" />
+      </el-form-item-with-error>
+      <el-form-item-with-error label="Фотография" name="is_checked" :errors="errors">
+        <el-checkbox v-model="formData.is_checked" label="Согласен с условиями пользовательского соглашения" size="large" />
       </el-form-item-with-error>
     </el-form>
   </FormModal>
